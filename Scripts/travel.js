ecb33cd3c5d603da1c4d1d15cb1cce8d7035fb73
Rakibul <mdrakibulhassan2425@gmail.com>
2024-02-16 21:22:56 +0600

    add total cost functionality

--- Scripts/travel.js
+++ Scripts/travel.js
@@ -3,17 +3,39 @@ function getClass (classes) {
     const elems = document.getElementsByClassName(classes);
     return elems
 }
-// Get all card add to card buttons 
-const allAddToCardBtn = getClass("card-btn");
-let cardCount = 0;
-for (const addBtn of allAddToCardBtn) {
-    addBtn.addEventListener("click",function (e) {
-        cardCount += 1;
-        getText ("productList",cardCount)
-    })
-}
 // function for getInnerText and setInnerText
 function getText (id,value) {
     const getID = document.getElementById(id);
     getID.innerText = value;
+}
+// function for create dom elements 
+function makeElem (elemName) {
+    const createElem = document.createElement(elemName);
+    return createElem;
+}
+// Get all  add to card buttons 
+const allAddToCardBtn = getClass("card-btn");
+let cardCount = 0;
+for (const addBtn of allAddToCardBtn) {
+    addBtn.addEventListener("click",function (e) {
+        const placeName = e.target.parentNode.parentNode.childNodes[1].innerText;
+        const price = e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+        cardCount += 1;
+        getText ("productList",cardCount);
+        // create li 
+        const li = makeElem("li");
+        li.innerText = `${placeName} ${price}$`;
+        // append inside of ol 
+        const ol = document.getElementById("placeList");
+        ol.appendChild(li);
+        const totalCost = document.getElementById("totalCost").innerHTML;
+        addTotalCost("totalCost",totalCost,price)  
+    })
+}
+// function for add total cost 
+function addTotalCost (id,prevusCost,currentCost) {
+    const totalCostNumber = parseInt(prevusCost);
+    const priceNumber = parseInt(currentCost);
+    const sumTotalCost = totalCostNumber + priceNumber;
+    getText(id,sumTotalCost)
 }
